c2748bd8183e168bcb0b622859c408c94c7c0161
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-06-20 20:53:37 +1000

    Update syntax to async await

--- index.js
+++ index.js
@@ -7,14 +7,13 @@ const conn = new jsforce.Connection({
   redirectUri: 'https://localhost:3000/oauth/_callback'
 })
 
-conn.login(process.env.SALESFORCE_USERNAME, process.env.SALESFORCE_PASSWORD+process.env.SALESFORCE_SECURITY_TOKEN, function(err, res) {
-  if (err) {
-    return console.error(err)
+const run = async() => {
+  try {
+    const login = await conn.login(process.env.SALESFORCE_USERNAME, process.env.SALESFORCE_PASSWORD+process.env.SALESFORCE_SECURITY_TOKEN)
+    const result = await conn.query('SELECT Id, Name FROM Account')
+    console.log(result)
+  } catch(error) {
+    console.error(error)
   }
-  conn.query('SELECT Id, Name FROM Account', function(err, res) {
-    if (err) {
-      return console.error(err)
-    }
-    console.log(res)
-  })
-})
+}
+run()
